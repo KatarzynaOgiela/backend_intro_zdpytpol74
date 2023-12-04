7dd9693285c316e3cdfc1afd4f91b1f23792bb01
Katarzyna Ogiela <Katarzyna.ogiela96@gmail.com>
2023-12-04 19:45:01 +0100

    django, urls, settings

---
+++ hello/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from hello.views import hello_view, hello_eva, hello_adam, hello_name
+
+urlpatterns = [
+    path('', hello_view),
+    path('eva/', hello_eva),
+    path('adam/', hello_adam),
+    path('<str:name>/', hello_name),
+]
---
+++ hello/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import HttpResponse
+
+
+def hello_view(request):
+    return HttpResponse("Hello, world!")
+
+
+def hello_eva(request):
+    return HttpResponse("Hello, Eva!")
+
+
+def hello_adam(request):
+    return HttpResponse("Hello, Adam!")
+
+
+def hello_name(request, name):
+    return HttpResponse(f"Hello, {name.title()}! Nice to see you!")
