7f9b7193345d0367e369b12cd98b80f9e873d127
Katarzyna Ogiela <Katarzyna.ogiela96@gmail.com>
2023-12-05 20:48:22 +0100

    updates link

--- link_app/views.py
+++ link_app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+
 def first_view(request):
     return render(
         request,
         'first.html'
+    )
+
+
+def second_view(request):
+    return render(
+        request,
+        'second.html'
+    )
+
+
+def third_view(request, param):
+    return render(
+        request,
+        'third.html',
+        {
+            'param': param
+        }
     )
