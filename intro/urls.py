51301b4b25145fcf9f6b94ac32da3566ce96539b
Katarzyna Ogiela <Katarzyna.ogiela96@gmail.com>
2023-12-06 19:48:50 +0100

    layout

--- intro/urls.py
+++ intro/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('template/', include('template_app.urls')),
     path('link/', include('link_app.urls')),
     path('inheritance/', include('inheritance_app.urls')),
+    path('form1/', include('form_app1.urls')),
 ]
