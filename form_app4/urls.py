a99f296d109c62ef14c60fec7939e1b94a785000
Katarzyna Ogiela <Katarzyna.ogiela96@gmail.com>
2023-12-07 18:04:27 +0100

    detail

--- form_app4/urls.py
+++ form_app4/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from form_app4.views import task_create_view, task_list_view
+from form_app4.views import task_create_view, task_list_view, task_detail_view
 
 app_name = 'form_app4'
 
 
 urlpatterns = [
-    path('task/create/', task_create_view, name='create_task'),
-    path('task/', task_list_view, name='list_task'),
+    # C z crud
+    path('tasks/create/', task_create_view, name='create_task'),
+
+    # R z crud LISTA
+    path('tasks/', task_list_view, name='list_task'),
+
+    # R z crud SZCEGOL
+    path('tasks/<int:task_id>/', task_detail_view, name='list_detail')
 ]
--- form_app4/views.py
+++ form_app4/views.py
@@ -1,29 +1,49 @@
 # formularz metoda post
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, Http404
 
 TASKS = []
 
+
 # CRUD _ Create Read Update Delete
+# C
 def task_create_view(request):
     if request.method == "GET":
         return render(
             request,
             'form_app4/task_form.html',
             {
-                'task':TASKS
+                'tasks': TASKS
             }
         )
     if request.method == "POST":
         task = request.GET.get('task')
         if task is not None:
             TASKS.append(task)
+
         return redirect('form_app4:task_list')
 
+
+# R
 def task_list_view(request):
-        return render(
-            request,
-            'form_app4/task_form.html',
-            {
-                'task':TASKS
-            }
-        )
+    return render(
+        request,
+        'form_app4/task_list.html',
+        {
+            'tasks': TASKS
+        }
+    )
+
+
+def task_detail_view(request, task_id):
+    if task_id > len(TASKS):
+        raise Http404()
+
+    task =TASKS[task_id-1]
+    return render(
+        request,
+        'form_app4/task_detail.html',
+        {
+            'task_id': task_id,
+            'task': task
+        }
+    )
